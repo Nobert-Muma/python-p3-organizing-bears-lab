7b40131e0ef235fe263dcaa53370140abb3370b8
Nobert-Muma <mumanobert17@gmail.com>
2024-09-10 10:17:08 +0300

    Organizing Bears Lab.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 14, "M", "Light Skin", "Aggressive", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 13, "M", "Medium Skin", "Curious", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, "F", "Black", "Shy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Tan", "Gentle", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "White", "Curious", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Gentle", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "F", "Light Brown", "Aggressive", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "Dark Brown", "Curious", 1);
+ 
